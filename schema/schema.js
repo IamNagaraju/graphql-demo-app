2fcc934fcec65eaead682e86e4dc1e2c87522da6
IamNagaraju <pallipatinagarau@gmail.com>
2018-11-28 15:12:23 +0530

    models created

--- schema/schema.js
+++ schema/schema.js
@@ -1,19 +1,24 @@
-const { GraphQLObjectType ,GraphQLString,GraphQLInt ,GraphQLNonNull, GraphQLSchema, GraphQLList} = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLNonNull, GraphQLSchema, GraphQLList } = require('graphql');
 // console.log(GraphQLObjectType ,GraphQLString,GraphQLInt , GraphQLSchema,'string')
 // const _ = require('lodash');
+const User = require('../models/user');
+const Brand = require('../models/brand');
+
 // install json-server for db.json file
 const axios = require('axios');
 
 const BrandType = new GraphQLObjectType({
     name: 'Brand',
     fields: () => ({
-        id:{type: GraphQLString},
-        name: {type: GraphQLString},
-        description: {type: GraphQLString},
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
         users: {
             type: new GraphQLList(UserType),
-            resolve(parentValue,args) {
-                return axios.get(`http://localhost:3000/brands/${parentValue.id}/users`).then(res => res.data)
+            resolve(parentValue, args) {
+                console.log(parentValue)
+                return Brand.find({brandId:parentValue.id})
+                // return axios.get(`http://localhost:3000/brands/${parentValue.id}/users`).then(res => res.data)
             }
         }
     })
@@ -22,16 +27,17 @@ const BrandType = new GraphQLObjectType({
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: () => ({
-        id: {type: GraphQLString}, 
-        firstName: {type: GraphQLString},
-        age: {type: GraphQLInt},
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
         brand: {
             type: BrandType,
-            resolve(parentValue,args) {
+            resolve(parentValue, args) {
                 console.log(parentValue.brandId)// parentvalue will have user data
-                return axios.get(`http://localhost:3000/brands/${parentValue.brandId}`).then(res => res.data)
+                return Brand.findById(parentValue.brandId)
+                // return axios.get(`http://localhost:3000/brands/${parentValue.brandId}`).then(res => res.data)
             }
-        } 
+        }
     })
 })
 
@@ -44,31 +50,49 @@ const UserType = new GraphQLObjectType({
 
 // Without using root it is difficult to find the data. Root query is the entry point into our application.
 //to find the exact what we want 
-const RootQuery = new GraphQLObjectType ({
+const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         user: {
             type: UserType,
             // args are the find the match data example is finding through id
-            args: {id: {type:GraphQLString} },
+            args: { id: { type: GraphQLString } },
             //resolve is used find the data which is passed in the args from database
-            resolve(parentValue,args) {
+            resolve(parentValue, args) {
+                return User.findById(args.id)
                 // return _.find(users, {id: args.id})  use for static data with lodash method
-                return axios.get(`http://localhost:3000/users/${args.id}`).then(response => response.data)
-
+                // return axios.get(`http://localhost:3000/users/${args.id}`).then(response => response.data)
+                
             }
         },
         brand: {
             type: BrandType,
             // args are the find the match data example is finding through id
-            args: {id: {type:GraphQLString} },
+            args: { id: { type: GraphQLString } },
             //resolve is used find the data which is passed in the args from database
-            resolve(parentValue,args) {
+            resolve(parentValue, args) {
+                return Brand.findById(args.id);
                 // return _.find(users, {id: args.id})  use for static data with lodash method
-                return axios.get(`http://localhost:3000/brands/${args.id}`).then(response => response.data)
+                // return axios.get(`http://localhost:3000/brands/${args.id}`).then(response => response.data)
 
             }
-        }
+        },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue,args) {
+                return User.find({})
+                // return axios.get(`http://localhost:3000/users`).then(res => res.data)
+                // return axios.get(`https://app-store32.myshopify.com/admin/shop.json`).then(res => res.data)
+            }
+        },
+        brands: {
+            type: new GraphQLList(BrandType),
+            resolve(parentValue,args) {
+                return Brand.find({})
+                // return axios.get(`http://localhost:3000/brands`).then(res => res.data)
+            }
+        },
+
     }
 })
 
@@ -80,17 +104,66 @@ const mutation = new GraphQLObjectType({
             type: UserType,
             args: {
                 firstName: { type: new GraphQLNonNull(GraphQLString) },
-                age: {type: new GraphQLNonNull(GraphQLInt)},
-                brandId: { type: GraphQLString}
+                age: { type: new GraphQLNonNull(GraphQLInt) },
+                brandId: { type: GraphQLString }
             },
-            resolve(parentValue, { firstName , age }) {
-                return axios.post('http://localhost:3000/users', { firstName , age }).then(res => res.data)
+            resolve(parentValue, args) {
+                console.log(args)
+                // return axios.post('http://localhost:3000/users', { firstName, age }).then(res => res.data)
+                let user = new User({
+                    firstName:args.firstName,
+                    age: args.age
+                })
+               return user.save()
+            }
+        },
+
+        addBrand: {
+            type: BrandType,
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                description: { type: new GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parentValue, args) {
+                console.log(args)
+                // return axios.post('http://localhost:3000/users', { firstName, age }).then(res => res.data)
+                let brand = new Brand({
+                    name:args.name,
+                    description: args.description
+                })
+               return brand.save()
+            }
+        },
+
+        deleteUser: {
+            type: UserType,
+            args: {
+                id: {
+                    type: new GraphQLNonNull(GraphQLString)
+                }
+            },
+            resolve(parentValue, args) {
+                return axios.delete(`http://localhost:3000/users/${args.id}`).then(res => res.data)
+            }
+        },
+        updateUser: {
+            type: UserType,
+            args: {
+                id: {
+                    type: new GraphQLNonNull(GraphQLString)
+                },
+                firstName: { type: GraphQLString },
+                age: { type: GraphQLInt },
+                brandId: { type: GraphQLString },
+            },
+            resolve(parentValue, args) {
+                return axios.patch(`http://localhost:3000/users/${args.id}`,args).then(res => res.data)
             }
         }
     }
 })
 
-module.exports  = new GraphQLSchema({
+module.exports = new GraphQLSchema({
     query: RootQuery,
     mutation
 })
--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose')
 // Create an express server and a GraphQl Endpoint
 
 const app = express();
+
+mongoose.connect('mongodb://Nagaraju:newlook581@ds151393.mlab.com:51393/lyricaldb',{useNewUrlParser:true});
+
+mongoose.connection.once('open',() => {
+    console.log('connected to database');
+})
+
 app.use('/graphql',expressGraphQL({
     schema,
     // rootValue: root,
     graphiql: true //used to make queries against our development servers
 }))
 
-app.listen(4000, () => {
-    console.log('listing to  grpahql port '+ 4000);
+app.listen(4001, () => {
+    console.log('listing to  grpahql port '+ 4001);
 })
